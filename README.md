9ffeab81adca5e49fd1ed555cfc3dc6d55d483f1
Ayanrocks <ayanb1999@gmail.com>
2020-01-16 13:03:04 +0530

    Added proto files and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # gRPC-jwt-auth
 gRPC authentication using JWT
+
+
+## Generate protobuf file for GRPC Go client
+```shell
+$  protoc --proto_path=protos --go_out=plugins=grpc:protos conn.proto
+```
+
+## Generate protobuf for Python GRPC server
+```shell
+$ python -m grpc_tools.protoc -I./protos --python_out=./generated/ --grpc_python_out=./generated/ ./protos/conn.proto
+```
+
+Run Python Server using
+```shell
+$ python3 server.py
+```
+
+While python server running, Run the ```main.go``` file
+```shell
+$ go run main.go
+```
--- grpc-python-server/server.py
+++ grpc-python-server/server.py
@@ -2,19 +2,19 @@ import sys
 sys.path.insert(1, './generated/')
 import logging
 import grpc
-import client_call_pb2
-import client_call_pb2_grpc
+import conn_pb2
+import conn_pb2_grpc
 from concurrent import futures
 from google.protobuf.json_format import MessageToJson
 import requests
 from request_header_validator_interceptor import RequestHeaderValidatorInterceptor
 
 
-class Deploy(client_call_pb2_grpc.ClientCallServiceServicer):
+class Deploy(conn_pb2_grpc.ClientCallServiceServicer):
     def RunDeploy(self, request, context):
         print("Running command: ", request.callInterface.command)
         if request.callInterface.command == "deploy-contract":
-            resp = client_call_pb2.ClientCallResponse(result="success")
+            resp = conn_pb2.ClientCallResponse(result="success")
             yield resp
         else:
             return
@@ -22,7 +22,7 @@ class Deploy(client_call_pb2_grpc.ClientCallServiceServicer):
 def serve():
     header_validator = RequestHeaderValidatorInterceptor(grpc.StatusCode.UNAUTHENTICATED, 'Access denied!')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), interceptors=(header_validator,))
-    client_call_pb2_grpc.add_ClientCallServiceServicer_to_server(Deploy(), server)
+    conn_pb2_grpc.add_ClientCallServiceServicer_to_server(Deploy(), server)
     server.add_insecure_port('[::]:50053')
     server.start()
     print("gRPC server listening on port 50053")
